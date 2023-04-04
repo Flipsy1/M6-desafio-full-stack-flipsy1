1ba1b2a68c5b715511fb3f6a22aa8e5f7aa0bd49
Flipsy1 <felipevie@hotmail.com>
2023-04-04 13:17:59 -0400

    correção cors

--- back/src/app.ts
+++ back/src/app.ts
@@ -4,9 +4,11 @@ import { handleError } from "./erros";
 import loginRoutes from "./routes/login.routes";
 import usersRoutes from "./routes/users.routes";
 import contactsRoutes from "./routes/contacts.routes";
+import cors from "cors";
 
 const app: Application = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/users", usersRoutes);
 app.use("/login", loginRoutes);
